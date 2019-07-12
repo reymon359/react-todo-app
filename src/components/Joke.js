f09ff949b1b8e86521f49816bd4890b8f4151bb5
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-12 12:14:39 +0200

    joke component

---
+++ src/components/Joke.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Joke(props) {
+    return (
+        <div>
+            <h3>Question: </h3>
+            <h3>Answer: </h3>
+        </div>
+    )
+}
+
+export default Joke
