ca13159e8113071abb698bf130952cc561b6fafe
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-12 11:27:40 +0200

    added stiles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { TodoItem } from "./TodoItem";
+import TodoItem from './TodoItem';
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="todo-list">
       <TodoItem />
       <TodoItem />
       <TodoItem />
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 function TodoItem(){
 
     return (
-        <div>
+        <div className="todo-item">
             <input type="checkbox" />
             <p>Placeholder text</p>
         </div>
@@ -11,4 +11,4 @@ function TodoItem(){
 
 }
 
-export default TodoItem
+export default TodoItem;
