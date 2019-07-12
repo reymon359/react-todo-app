5181edc8c7f32dfa530f6c280b77ee75cda4db9a
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-12 12:07:39 +0200

    adding stuff from the course

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,23 @@ import React from 'react'
 
 function Header(){
 
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    
+    if (hours < 12) {
+      timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+    } else {
+      timeOfDay = "night"
+    }
+
     return (
-        <header className="navbar">Todo App </header>
+        <header className="navbar">
+            <div>Todo App</div> 
+            <small>Good {timeOfDay}! Here you can write your tasks</small>
+            </header>
     )
 
 }
