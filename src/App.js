00cef98168140fd2a12aa64d9685d440b03fe84d
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-12 11:37:43 +0200

    adding header to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+
+// Components
+import Header from './components/Header';
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
   return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+    
+    <div>
+      <Header />
+      <div className="todo-list">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
     </div>
+
   );
 }
 
