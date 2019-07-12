657b0dc3519729df636dd6f57a0016874cd5407e
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-12 11:17:47 +0200

    todo item component created

---
+++ src/TodoItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TodoItem(){
+
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Placeholder text</p>
+        </div>
+    )
+
+}
+
+export default TodoItem
