3cf5fd4bc33b61ed516b049f5faffa26e43b25b3
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-12 12:27:29 +0200

    ordering components

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import TodoItem from './TodoItem';
 
 // Components
 import Header from './components/Header';
+import Jokes from './components/jokes/Jokes';
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
-  return (
-    
-    <div>
-      <Header />
-      <div className="todo-list">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-      </div>
-    </div>
+    return (
 
-  );
+        <
+        div >
+        <
+        Header / >
+        <
+        div className = "todo-list" >
+        <
+        TodoItem / >
+        <
+        TodoItem / >
+        <
+        TodoItem / >
+        <
+        TodoItem / >
+        <
+        /div> <
+        Jokes / >
+        <
+        /div>
+
+    );
 }
 
 export default App;
