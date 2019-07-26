f825e78b83eaede1cfbfc394e1a6fc4609a8141b
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-26 12:07:02 +0200

    changing the state of the todos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import todosData from './todosData';
 // Components
 import Header from './components/Header';
 import Jokes from './components/jokes/Jokes';
+import { runInThisContext } from 'vm';
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -14,10 +15,30 @@ class App extends React.Component {
     this.state = {
         todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
+  // Creating a new array where if the item has the same id as the
+  // one passed in the params we change its completed value
+  handleChange(id) {
+    
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+          if (todo.id === id) {
+              todo.completed = !todo.completed
+          }
+          return todo
+      })
+      return {
+          todos: updatedTodos
+      }
+    })
+  }
+  
   render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}
+    handleChange={this.handleChange }
+    />)
     
     return (
       <div>
