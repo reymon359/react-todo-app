2c3d08ab2ed2260414d33ead6a835d3c3f4ca465
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-08-03 16:59:34 +0200

    Updates

---
+++ precache-manifest.f0fc9a350e9f97dc80d3e2464a186bfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135fef8eb4226000bf85ceda57b6e65f",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "faf3eda77860e43a61bb",
+    "url": "/react-todo-app/static/css/main.1fbcc6cb.chunk.css"
+  },
+  {
+    "revision": "e77c4d54a36111f6e232",
+    "url": "/react-todo-app/static/js/2.45f59c9b.chunk.js"
+  },
+  {
+    "revision": "faf3eda77860e43a61bb",
+    "url": "/react-todo-app/static/js/main.906194fb.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
