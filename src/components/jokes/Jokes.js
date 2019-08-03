a387c8a876bf7b51dbfb5f9b4efcd2dca8e2c751
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-08-03 17:04:07 +0200

    fixed jokes bug

--- src/components/jokes/Jokes.js
+++ src/components/jokes/Jokes.js
@@ -4,12 +4,13 @@ import Joke from "./Joke"
 
 import jokesData from "./jokesData"
 
+// Using map to go through all the jokes in jokesData
+const jokeComponents = jokesData.sort(() => Math.random() - 0.5).slice(0, 2).map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
+ 
+         
 function Jokes() {
     
-    // Using map to go through all the jokes in jokesData
-    const jokeComponents = jokesData.sort(() => Math.random() - 0.5).slice(0, 2).map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
- 
-                
+          
     return (
         <div  className="jokes">
             <b>By the way here are some jokes😄.</b>
